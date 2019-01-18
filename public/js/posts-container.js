b979f9ee1a065663d17639033e32f7ef1ee9acc9
booobi <qqwweee59@gmail.com>
2019-01-18 14:27:45 +0200

    Remove extra container from main grid column, Add (temporary) 150px margin to posts in PostsContainer

--- public/js/posts-container.js
+++ public/js/posts-container.js
@@ -19,7 +19,7 @@ class PostsContainer extends React.Component {
       const postSummaries = this.state.postsData.map((postData) => <PostPreview postTitle={postData.postTitle} postSubtitle={postData.postSubtitle}/>)
       
       return (
-        <div>
+        <div style={{paddingLeft:'150px'}}>
             {postSummaries}
           <div className="clearfix">
             <a className="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
